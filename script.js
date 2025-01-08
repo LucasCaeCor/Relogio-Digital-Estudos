9c07e61beb5d809f4707bda59544944e5bf1d539
Lucas Caetano Cordeiro <146130536+LucasCaeCor@users.noreply.github.com>
2025-01-08 17:09:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const $ = (selector) => {
+    return document.querySelector(selector); 
+}
+
+const hour = $('.hour');
+const dot = $('.dot');
+const min = $('.min');
+const week = $('.week');
+let showDot = true;
+
+function PaymentRequestUpdateEvent() {
+    showDot = !showDot;
+    const now = new Date();
+
+    if (showDot) {
+        dot.classList.add('invisible'); 
+    } else {
+        dot.classList.remove('invisible');
+    }
+
+    hour.textContent = String(now.getHours()).padStart(2, '0');
+    min.textContent = String(now.getMinutes()).padStart(2, '0');
+
+    Array.from(week.children).forEach(
+        (ele) => {
+            ele.classList.remove('active');
+        }
+    );
+
+    week.children[now.getDay()].classList.add('active');
+}
+
+
+setInterval(PaymentRequestUpdateEvent, 500);
